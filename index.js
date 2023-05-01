8cb53b92e12b1f70797731e3a9c0b1453db6f504
saicharanars <saicharan_@outlook.com>
2023-05-01 18:23:42 +0530

    jsprojecttest

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function expense(event) {
   event.preventDefault();
+  let api = "https://crudcrud.com/api/558a45ed74b04ad8b5dab7c6ebd44d66";
   const amount = document.getElementById("amount").value;
   const etype = document.getElementById("etype").value;
   const cat = document.getElementById("cat").value;
@@ -8,31 +9,58 @@ function expense(event) {
     etype,
     cat,
   };
-  localStorage.setItem(obj.etype, JSON.stringify(obj));
-  showexpenseonscreen(obj);
+
+  axios
+    .post(`${api}/expense`, obj)
+    .then((res) => {
+      showexpenseonscreen(res.data);
+    })
+    .catch((err) => console.error(err));
 }
+window.addEventListener("DOMContentLoaded", () => {
+  let api = "https://crudcrud.com/api/558a45ed74b04ad8b5dab7c6ebd44d66";
+
+  axios.get(`${api}/expense`).then((res) => {
+    for (var i = 0; i < res.data.length; i++) {
+      showexpenseonscreen(res.data[i]);
+      console.log(res.data[i].cat);
+    }
+  });
+});
 function showexpenseonscreen(obj) {
+  let api = "https://crudcrud.com/api/558a45ed74b04ad8b5dab7c6ebd44d66/expense";
   const parentele = document.getElementById("listofitems");
+  const grocery = document.getElementById("grocery");
+  const maintenance = document.getElementById("maintenance");
+  const entertainment = document.getElementById("entertainment");
   const childele = document.createElement("li");
   childele.textContent = obj.amount + "-" + obj.cat + "-" + obj.etype;
   const deletebtn = document.createElement("input");
   deletebtn.type = "button";
   deletebtn.value = "Delete";
   deletebtn.onclick = () => {
-    localStorage.removeItem(obj.etype);
-    parentele.removeChild(childele);
-  };
-  const editbtn = document.createElement("input");
-  editbtn.type = "button";
-  editbtn.value = "Edit";
-  editbtn.onclick = () => {
-    localStorage.removeItem(obj.email);
-    parentele.removeChild(childele);
-    document.getElementById("amount").value = obj.amount;
-    document.getElementById("etype").value = obj.etype;
+    axios.delete(`${api}/${obj._id}`);
+    if (obj.cat === "grocery") {
+      grocery.removeChild(childele);
+    } else if (obj.cat === "entertainment") {
+      entertainment.removeChild(childele);
+    } else {
+      maintenance.removeChild(childele);
+    }
+    //localStorage.removeItem(obj.etype);
+    //parentele.removeChild(childele);
   };
 
   childele.appendChild(deletebtn);
-  childele.appendChild(editbtn);
-  parentele.appendChild(childele);
+  //childele.appendChild(editbtn);
+  //grocery.appendChild(childele);
+  //parentele.appendChild(childele);
+
+  if (obj.cat === "grocery") {
+    grocery.appendChild(childele);
+  } else if (obj.cat === "entertainment") {
+    entertainment.appendChild(childele);
+  } else {
+    maintenance.appendChild(childele);
+  }
 }

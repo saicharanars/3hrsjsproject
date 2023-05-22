753a2f9f161082e3ee9bbefbf5075ecd346a00a5
saicharanars <saicharan_@outlook.com>
2023-05-22 02:34:27 +0100

    nochange

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var api = "https://crudcrud.com/api/381b1154c91d49e7aa77c02ac54bc0ab";
-function expense(event) {
+var api = "https://crudcrud.com/api/0fa1b94261b84e9db77b6185b01e239c";
+async function expense(event) {
   event.preventDefault();
   const amount = document.getElementById("amount").value;
   const etype = document.getElementById("etype").value;
@@ -10,28 +10,32 @@ function expense(event) {
     cat,
   };
 
-  axios
-    .post(`${api}/expense`, obj)
-    .then((res) => {
-      showexpenseonscreen(res.data);
-    })
-    .catch((err) => console.error(err));
-  // try {
-  //   const res = await axios.post(`${api}/expense`, obj);
-  //   showexpenseonscreen(res.data);
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  // axios
+  //   .post(`${api}/expense`, obj)
+  //   .then((res) => {
+  //     showexpenseonscreen(res.data);
+  //   })
+  //   .catch((err) => console.error(err));
+  try {
+    const res = await axios.post(`${api}/expense`, obj);
+    showexpenseonscreen(res.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
-window.addEventListener("DOMContentLoaded", () => {
-  
+window.addEventListener("DOMContentLoaded", async() => {
+  try {
+    const res = await axios.get(`${api}/expense`);
 
-  axios.get(`${api}/expense`).then((res) => {
-    for (var i = 0; i < res.data.length; i++) {
+    for (let i = 0; i < res.data.length; i++) {
       showexpenseonscreen(res.data[i]);
-      console.log(res.data[i].cat);
     }
-  });
+    
+  } catch (error) {
+    console.log(error)
+  }
+  
+    
 });
 function showexpenseonscreen(obj) {
   
@@ -40,25 +44,39 @@ function showexpenseonscreen(obj) {
   const maintenance = document.getElementById("maintenance");
   const entertainment = document.getElementById("entertainment");
   const childele = document.createElement("li");
-  childele.className ="list-group-item"
+  childele.className ="list-group-item";
   childele.textContent = obj.amount + "   " + obj.cat + "   " + obj.etype;
   
   const deletebtn = document.createElement("input");
   deletebtn.type = "button";
   deletebtn.value = "Delete";
   deletebtn.className="btn btn-outline-danger float-end"
-  deletebtn.onclick = () => {
+  deletebtn.onclick = async() => {
     
-    axios.delete(`${api}/expense/${obj._id}`
-    ).then(() =>{
-      if (obj.cat === "grocery") {
-        grocery.removeChild(childele);
-      } else if (obj.cat === "entertainment") {
-        entertainment.removeChild(childele);
-      } else {
-        maintenance.removeChild(childele);
-      }
-    }).catch((err)=>console.log(err));
+    // axios.delete(`${api}/expense/${obj._id}`
+    // ).then(() =>{
+    //   if (obj.cat === "grocery") {
+    //     grocery.removeChild(childele);
+    //   } else if (obj.cat === "entertainment") {
+    //     entertainment.removeChild(childele);
+    //   } else {
+    //     maintenance.removeChild(childele);
+    //   }
+    // }).catch((err)=>console.error(err));
+    try {
+    const res = await axios.delete(`${api}/expense/${obj._id}`);
+    console.log(res);
+    if (obj.cat === "grocery") {
+      grocery.removeChild(childele);
+    } else if (obj.cat === "entertainment") {
+      entertainment.removeChild(childele);
+    } else {
+      maintenance.removeChild(childele);
+    }
+    } catch (error) {
+    console.log(error);
+     }
+    
     
     //localStorage.removeItem(obj.etype);
     //parentele.removeChild(childele);
